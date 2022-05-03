654b6abf5eee27e82fe26444dc1da7d2b658c0e6
Fernando <fernandoalzamendi@gmail.com>
2022-05-03 15:54:01 -0300

    new db

--- book_keeper/db/book_keeper.sql
+++ book_keeper/db/book_keeper.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-04-2022 a las 20:53:53
+-- Tiempo de generación: 03-05-2022 a las 20:52:39
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 7.3.26
 
@@ -24,6 +24,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `amigos`
+--
+
+CREATE TABLE `amigos` (
+  `id_amigo` int(11) NOT NULL,
+  `amigo` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `amigos`
+--
+
+INSERT INTO `amigos` (`id_amigo`, `amigo`) VALUES
+(1, 'PEPE');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `books`
 --
 
@@ -31,7 +49,7 @@ CREATE TABLE `books` (
   `id` int(11) NOT NULL,
   `titulo` varchar(50) NOT NULL,
   `autor` varchar(50) NOT NULL,
-  `lector` varchar(50) NOT NULL,
+  `id_amigo` int(11) NOT NULL,
   `borrowed_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -39,18 +57,48 @@ CREATE TABLE `books` (
 -- Volcado de datos para la tabla `books`
 --
 
-INSERT INTO `books` (`id`, `titulo`, `autor`, `lector`, `borrowed_date`) VALUES
-(0, 'La muralla china y otros cuentos', 'Kafka', '', '2022-04-18');
+INSERT INTO `books` (`id`, `titulo`, `autor`, `id_amigo`, `borrowed_date`) VALUES
+(0, 'La muralla china y otros cuentos', 'Kafka', 1, '2022-04-18');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `amigos`
+--
+ALTER TABLE `amigos`
+  ADD PRIMARY KEY (`id_amigo`),
+  ADD KEY `id_amigo` (`id_amigo`);
+
+--
 -- Indices de la tabla `books`
 --
 ALTER TABLE `books`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_2` (`id`),
+  ADD KEY `id` (`id`),
+  ADD KEY `id_amigo` (`id_amigo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `amigos`
+--
+ALTER TABLE `amigos`
+  MODIFY `id_amigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `books`
+--
+ALTER TABLE `books`
+  ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`id_amigo`) REFERENCES `amigos` (`id_amigo`) ON DELETE NO ACTION ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- book_keeper/index.php
+++ book_keeper/index.php
@@ -210,13 +210,7 @@
           </table>
           <hr>
 
-
-
-
-
-
-
-
+          
           <div class="w3-container">
             <h5 class="w3-opacity"><b>W3Schools.com</b></h5>
             <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Forever</h6>
